6eeab858cb64a0f107a681e1adcc1f007323cfff
Guilherme <guicardosodeoliveira@gmail.com>
2023-06-27 09:25:51 -0300

    responsividade tela pedido confirmado e alteração na cor do svg

--- src/app/pedido-confirmado/page.tsx
+++ src/app/pedido-confirmado/page.tsx
@@ -5,46 +5,47 @@ import { BsCurrencyDollar } from "react-icons/bs";
 
 export default function PedidoConfirmado() {
   return (
-    <section className="flex flex-row-reverse justify-center items-center gap-20 mt-28">
-      <div>
-        <div className="py-8">
-          <h1 className="text-2xl font-bold mb-2">Uhu! Pedido confirmado</h1>
+    <section className="flex flex-col lg:flex-row-reverse justify-center items-center gap-20 lg:mt-28">
+      <div className="flex flex-col justify-center items-center">
+        <div className="py-8 text-center lg:text-start">
+          <h1 className="text-lg lg:text-2xl font-bold mb-2">Uhu! Pedido confirmado</h1>
           <p>Agora é só aguardar que seu pedido chegará até você</p>
         </div>
-        <div className="w-full p-12 border border-black border-solid rounded-tl-3xl rounded-br-3xl">
+        <div className="lg:w-full w-4/5 p-12 border border-black border-solid rounded-tl-3xl rounded-br-3xl">
           <ul className="flex flex-col gap-5">
             <li className="flex items-center gap-3">
               <div>
-                <MdLocationOn className="text-2xl" />
+                <MdLocationOn className="lg:text-2xl text-xl" />
               </div>
               <div>
-                <p>Entregue em Rua Felipe Domingues Petry, 600</p>
-                <p>Praia Comprida-SC</p>
-              </div>
-            </li>
-            <li className="flex items-center gap-3">
-              <div>
-                <MdAccessTimeFilled className="text-xl" />
-              </div>
-              <div>
-                <p>Previsão de Entrega</p>
-                <p>20 - 30 Minutos</p>
+                <p className="lg:text-base text-sm">Entregue em Rua Felipe Domingues Petry, 600</p>
+                <p className="lg:text-base text-sm">Praia Comprida-SC</p>
               </div>
             </li>
             <li className="flex items-center gap-3">
               <div>
-                <BsCurrencyDollar className="text-2xl" />
+                <MdAccessTimeFilled className="lg:text-xl text-lg" />
               </div>
               <div>
-                <p>Pagamento na entrega</p>
-                <p>Dinheiro</p>
+                <p className="lg:text-base text-sm">Previsão de Entrega</p>
+                <p className="lg:text-base text-sm">20 - 30 Minutos</p>
+              </div>
+            </li>
+            <li className="flex items-center gap-3">
+              <div>
+                <BsCurrencyDollar className="lg:text-2xl text-xl" />
+              </div>
+              <div>
+                <p className="lg:text-base text-sm">Pagamento na entrega</p>
+                <p className="lg:text-base text-sm">Dinheiro</p>
               </div>
             </li>
           </ul>
         </div>
       </div>
-      <Image src={entregador} alt="Entregador"></Image>
-      <div></div>
+      <div>
+        <Image src={entregador} alt="Entregador"></Image>
+      </div>
     </section>
   );
 }
